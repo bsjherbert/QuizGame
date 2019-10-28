da2090a557bc6b8939449229aba1af3963f50302
Steven <bsjherbert@gmail.com>
2019-10-27 22:10:12 -0400

    Got the first page to switch to the second

--- main.js
+++ main.js
@@ -1,5 +1,52 @@
-var startBtn = document.getElementById("start-button");
+var startBtn = document.getElementById("startButton");
 
-startBtn/addEventListener("click", function(event) {
-    alert('boo');
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+  
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    }
+  
+  
+  
+  ];
+  
+
+startBtn.addEventListener("click", function(event) {
+    var x = document.getElementById("hideH1")
+    x.style.display = "none";
+    createQuestion(questions);
 });
+
+function createQuestion(question) {
+    for (let i = 0; i < question.length; i++) {
+         var x = document.createElement("button");
+        x.innerHTML = question[i].title;
+        var questionContainer = document.getElementById("questions");
+        questionContainer.appendChild(x);
+    }
+}
+
+
+
--- questions.js
+++ questions.js
@@ -1,14 +1,31 @@
 var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    ///etc.
-  ];
-  
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  }
+
+
+
+];
